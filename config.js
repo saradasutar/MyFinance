57d8de481f09ec7a85530d80d808e04b8cc5992c
Sarada <saradawrites@gmail.com>
2026-08-06 21:22:10 +0530

    Update API_URL to production Google Apps Script URL

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * The URL must end with /exec (not /dev).
  */
 window.PORTFOLIO_CONFIG = Object.freeze({
-  API_URL: 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxbqdU19I3Elgsj07hRUJtVs9vcUPGnTkNKzYDz5syolmm9dAos4CAu3FI_sAKNeihv/exec',
   APP_NAME: 'My Investment Dashboard',
   CURRENCY: 'INR',
   REQUEST_TIMEOUT_MS: 25000
