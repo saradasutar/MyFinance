5c54739d7ded7f382b62d0cfa723f0239326d1c8
Sarada <saradawrites@gmail.com>
2026-08-07 23:45:42 +0530

    Add PORTFOLIO_CONFIG to config.js
    Updated configuration to include PORTFOLIO_CONFIG with API_URL.

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
-window.APP_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbxbqdU19I3Elgsj07hRUJtVs9vcUPGnTkNKzYDz5syolmm9dAos4CAu3FI_sAKNeihv/exec'
+window.PORTFOLIO_CONFIG = {
+  API_URL: 'ttps://script.google.com/macros/s/AKfycbxbqdU19I3Elgsj07hRUJtVs9vcUPGnTkNKzYDz5syolmm9dAos4CAu3FI_sAKNeihv/exec'
+};
+
+  APP_NAME: 'My Finance',
+  CURRENCY: 'INR',
+  REQUEST_TIMEOUT_MS: 25000
 };
