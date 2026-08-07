ba9bebafe7cac6697d7850982f649a9f17ae3410
Sarada <saradawrites@gmail.com>
2026-08-07 23:58:24 +0530

    Fix syntax error in config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-
 window.PORTFOLIO_CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbxbqdU19I3Elgsj07hRUJtVs9vcUPGnTkNKzYDz5syolmm9dAos4CAu3FI_sAKNeihv/exec',
   APP_NAME: 'My Finance',
